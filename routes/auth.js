554f897adcfb9ca823578346d3f78472d618f3f1
divided7 <41837037+divided7@users.noreply.github.com>
2020-10-05 01:20:03 +0530

    ;)

--- routes/auth.js
+++ routes/auth.js
@@ -1,120 +1,124 @@
-const router = require("express").Router();
-const Team = require("../models/Team");
-const verify = require("../middleware/tokenVerification");
-const validateTeam = require("../middleware/teamValidate");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const axios = require("axios");
-var original = {};
-const { body } = require("express-validator");
+/* eslint-disable linebreak-style */
+const router = require('express').Router();
+const Team = require('../models/Team');
+const verify = require('../middleware/tokenVerification');
+const validateTeam = require('../middleware/teamValidate');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+let original = {};
+const {body} = require('express-validator');
 
 router.post(
-  "/register",
-  [body("name").trim().escape(), body("school").trim().escape()],
-  async (req, res) => {
-    var { error } = validateTeam(req.body);
-    if (error) {
-      original = error._original;
-      errors = [];
-      for (errorName of error.details) {
-        errors.push(errorName.path[0]);
+    '/register',
+    [body('name').trim().escape(), body('school').trim().escape()],
+    async (req, res) => {
+      const {error} = validateTeam(req.body);
+      if (error) {
+        original = error._original;
+        errors = [];
+        for (errorName of error.details) {
+          errors.push(errorName.path[0]);
+        }
+        return res.render('register.ejs', {
+          errors: errors,
+          original: original,
+        });
+      } else {
+        original = {
+          email: req.body.email,
+          password: req.body.password,
+          school: req.body.school,
+          discord: req.body.discord,
+          name: req.body.name,
+        };
       }
-      return res.render("register.ejs", {
-        errors: errors,
-        original: original,
+      response = await axios({
+        method: 'post',
+        url: 'https://www.google.com/recaptcha/api/siteverify',
+        params: {
+          secret: '6LefzcYZAAAAAOpTOSNQ8-JoBwfMeadgHgJt8F-O',
+          response: req.body['g-recaptcha-response'],
+        },
       });
-    } else {
-      original = {
-        email: req.body.email,
-        password: req.body.password,
-        school: req.body.school,
-        discord: req.body.discord,
-        name: req.body.name,
-      };
-    }
-    response = await axios({
-      method: "post",
-      url: "https://www.google.com/recaptcha/api/siteverify",
-      params: {
-        secret: "6LefzcYZAAAAAOpTOSNQ8-JoBwfMeadgHgJt8F-O",
-        response: req.body["g-recaptcha-response"],
-      },
-    });
-    var recap = response.data.success;
-    if (!recap) {
-      return res.render("register.ejs", {
-        galatRecaptcha: true,
-        original: original,
+      const recap = response.data.success;
+      if (!recap) {
+        return res.render('register.ejs', {
+          galatRecaptcha: true,
+          original: original,
+        });
+      }
+      const teamRegistered = await Team.findOne({email: req.body.email});
+      if (teamRegistered) {
+        return res.render('register.ejs', {alregistered: true});
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const hashed = await bcrypt.hash(req.body.password, salt);
+
+      const team = new Team({
+        school: req.body.school.trim(),
+        email: req.body.email.trim(),
+        password: hashed,
+        discord: req.body.discord.trim(),
+        name: req.body.name.trim(),
       });
-    }
-    const teamRegistered = await Team.findOne({ email: req.body.email });
-    if (teamRegistered)
-      return res.render("register.ejs", { alregistered: true });
-
-    const salt = await bcrypt.genSalt(10);
-    const hashed = await bcrypt.hash(req.body.password, salt);
-
-    const team = new Team({
-      school: req.body.school.trim(),
-      email: req.body.email.trim(),
-      password: hashed,
-      discord: req.body.discord.trim(),
-      name: req.body.name.trim(),
-    });
-    try {
-      const registered = await team.save();
-      res.render("register.ejs", { registered: true });
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  }
+      try {
+        const registered = await team.save();
+        res.render('register.ejs', {registered: true});
+      } catch (error) {
+        res.status(400).send(error);
+      }
+    },
 );
 
-router.post("/login", async (req, res) => {
-  const team = await Team.findOne({ email: req.body.email });
-  if (!team) return res.render("login.ejs", { failure: true, active: "login" });
+router.post('/login', async (req, res) => {
+  const team = await Team.findOne({email: req.body.email});
+  if (!team) return res.render('login.ejs', {failure: true, active: 'login'});
 
   const passCheck = await bcrypt.compare(req.body.password, team.password);
-  if (!passCheck)
-    return res.render("login.ejs", { failure: true, active: "login" });
+  if (!passCheck) {
+    return res.render('login.ejs', {failure: true, active: 'login'});
+  }
 
-  const token = jwt.sign({ _id: team._id }, process.env.JWT);
-  res.cookie("team", token, {
+  const token = jwt.sign({_id: team._id}, process.env.JWT);
+  res.cookie('team', token, {
     httpOnly: true,
     maxAge: 60 * 60 * 1000,
   });
-  res.redirect("/dashboard");
+  res.redirect('/dashboard');
 });
 
-router.post("/passchange", async (req, res) => {
-  const team = await Team.findOne({ email: req.body.email });
-  if (!team) return res.render("login.ejs", { failure: true, active: "login" });
+router.post('/passchange', async (req, res) => {
+  const team = await Team.findOne({email: req.body.email});
+  if (!team) return res.render('login.ejs', {failure: true, active: 'login'});
 
   const passCheck = await bcrypt.compare(req.body.old, team.password);
-  if (!passCheck)
-    return res.render("login.ejs", { failure: true, active: "login" });
+  if (!passCheck) {
+    return res.render('login.ejs', {failure: true, active: 'login'});
+  }
 
   const salt = await bcrypt.genSalt(10);
   const hashed = await bcrypt.hash(req.body.new, salt);
 
   Team.updateOne(
-    { email: req.body.email },
-    { $set: { password: hashed } },
-    { multi: true },
-    passcallback
+      {email: req.body.email},
+      {$set: {password: hashed}},
+      {multi: true},
+      passcallback,
   );
   function passcallback(err, num) {
     if (err) {
       console.log(err);
     } else {
-      return res.render("login.ejs", { passed: true, active: "login" });
+      return res.render('login.ejs', {passed: true, active: 'login'});
     }
   }
 });
 
-router.get("/logout", verify, (req, res) => {
-  res.cookie("team", "", { maxAge: 1 });
-  res.redirect("/");
+router.get('/logout', verify, (req, res) => {
+  res.cookie('team', '', {maxAge: 1});
+  res.redirect('/');
 });
 
 module.exports = router;
--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,35 +1,43 @@
 /* eslint-disable linebreak-style */
 // eslint-disable-next-line new-cap
-const router = require('express').Router();
-const verify = require('../middleware/tokenVerification');
-const Team = require('../models/Team');
+const router = require("express").Router();
+const verify = require("../middleware/tokenVerification");
+const Team = require("../models/Team");
 
-router.get('/leaderboard', verify, (req, res) => {
+router.get("/leaderboard", verify, (req, res) => {
   const success = req.query.success;
   Team.find({})
-      .sort({fp: 'desc'})
-      .exec(function(err, docs) {
-        if (err) {
-          console.log(err);
-        }
-        res.render('leaderboard.ejs', {
-          team: req.team,
-          allteams: docs,
-          active: 'leaderboard',
-          success: success,
-        });
+    .sort({ fp: "desc" })
+    .exec(function (err, docs) {
+      if (err) {
+        console.log(err);
+      }
+      res.render("leaderboard.ejs", {
+        team: req.team,
+        allteams: docs,
+        active: "leaderboard",
+        success: success,
       });
+    });
 });
 
-router.get('/leaderboardout', (req, res) => {
+router.get(
+  "/questions/shop/leaderboard/dashboard/supersecretpage",
+  verify,
+  (req, res) => {
+    res.render("flag.ejs");
+  }
+);
+
+router.get("/leaderboardout", (req, res) => {
   Team.find({})
-      .sort({fp: 'desc'})
-      .exec(function(err, docs) {
-        if (err) {
-          console.log(err);
-        }
-        res.render('leaders.ejs', {active: 'leaderboard', allteams: docs});
-      });
+    .sort({ fp: "desc" })
+    .exec(function (err, docs) {
+      if (err) {
+        console.log(err);
+      }
+      res.render("leaders.ejs", { active: "leaderboard", allteams: docs });
+    });
 });
 
 module.exports = router;
