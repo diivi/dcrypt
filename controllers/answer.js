8da4306d276f55160040c0df0d5fccc5ab3a2d0f
divided7 <41837037+divided7@users.noreply.github.com>
2020-10-10 12:27:39 +0530

    questionfiles

--- controllers/answer.js
+++ controllers/answer.js
@@ -3,36 +3,12 @@ const verify = require("../middleware/tokenVerification");
 const Team = require("../models/Team");
 const Log = require("../models/Log");
 const Questions = require("../models/Questions");
-var RateLimit = require("express-rate-limit");
 const mongoose = require("mongoose");
-var MongoStore = require("rate-limit-mongo");
 const Badboy = require("../models/Badboys");
 
 require("dotenv").config();
-var queslimiter = new RateLimit({
-  store: new MongoStore({
-    uri: process.env.MONGO_URI,
-    expireTimeMs: 60 * 1000 * 60,
-    collectionName: "ansForce",
-  }),
-  max: 150,
-  windowMs:10 * 60 * 1000,
-  message: "Too many requests in a short duration, IP Banned for an hour.",
-  onLimitReached: async function (req, res) {
-    try {
-      const badboy = new Badboy({
-        team: req.cookies.team,
-      });
-      try {
-        const bad = await badboy.save();
-      } catch (e) {
-        console.log(e);
-      }
-    } catch (e) {}
-  },
-});
 
-router.post("/answer/", verify, queslimiter, async (req, res) => {
+router.post("/answer/", verify, async (req, res) => {
   const activity = new Log({
     qtitle: req.body.title,
     sol: req.body.ans,
